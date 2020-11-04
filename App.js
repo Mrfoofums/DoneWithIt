d59e5f57e36f9794d891d5765f31a0ac0157a6bf
Forrest <forrest@longviewacquisitions.com>
2020-11-04 09:33:14 -0500

    welcome screen dinished

--- App.js
+++ App.js
@@ -2,40 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Dimensions, Text, View,TouchableWithoutFeedback,  Platform, Alert, TouchableOpacity, Button, TouchableHighlight,  Image, SafeAreaView } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 
 export default function App() {
   
 
   return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",//Sencondary axis, alignment of items within each line
-      // alignContent:"center"
-    }}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,
-       
-      }} />
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100
-      }} />
-
-    </View>
-
-
+    <WelcomeScreen/>
   );
 }
 
